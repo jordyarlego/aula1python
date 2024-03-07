bb384940feec8658b2e7b66c9ab8d4bb7d2048a6
jordyarlego <43409024+jordyarlego@users.noreply.github.com>
2024-03-07 11:11:07 -0300

    Add files via upload

---
+++ aulapython.py
@@ -0,0 +1,20 @@
+print('Olá Mundo!')
+nome='jordy'
+idade= 20
+altura=165
+input('Digite seu nome: ')
+if nome =='jordy':
+    print('você acertou o nome! kk')
+elif nome != 'jordy':
+    print('Você errou o nome!')
+
+input('Digite sua idade: ')
+if idade ==20:
+    print('Você acertou a idade')
+elif idade != 20:
+    print('Você errou a idade!')
+input('Digite sua altura: ')
+if altura== 165:
+    print('Você acertou a altura')
+elif altura != 165:
+    print('Você errou a altura!')        
