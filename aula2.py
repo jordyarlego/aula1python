500c6d4eed27aedd847936aeb30ead6fc8f2d527
jordyarlego <43409024+jordyarlego@users.noreply.github.com>
2024-03-19 10:23:24 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,29 @@
+idade1 = 22
+idade2 = 35
+print(idade1 + idade2)
+
+print(idade1 - idade2)
+
+print(idade1 * idade2)
+print(idade1 / idade2)
+print (idade1 * 14 / idade2 + 10 - idade1, ',é muita doideira papai kk')
+print (2*(idade1+idade2))
+print (2*(idade1 + 3* idade2 /4))
+print ((2*idade1+3*idade2)/4)
+print (idade1 ** idade2)
+
+calculo1 = 2*(idade1+idade2)
+print(calculo1)
+calculo2 = idade2 ** 2
+calculo3 = 15 **2 
+calculo4 = 15 **3
+print(calculo4)
+calculo5 = idade2 % idade1
+nome = 'fi'
+sobrenome = ' da peste!'
+print(nome + sobrenome)
+print ('Olá, ', nome, 'você tem ', idade1 +15, 'anos.')
+print('Eae fiquei sabendo que teu nome é: ', nome, '\n' 'e tu tem: ', idade2+40, 'anos de idade né, seu veio safado KK!!!')
+nome1 = str(input ('Digite seu nome vá, seu caba safado: '))
+idade3 = int(input('Digite sua idade, que to ligado já que tu é um veiaco: ')) 
+print ('Seu nome é : ', nome1, '\n' 'e sua idade é: ', idade3)
