3bd1b06ad009c4f424401d446e8c7e3817fe4ad4
jordyarlego <43409024+jordyarlego@users.noreply.github.com>
2024-03-26 10:35:27 -0300

    Add files via upload

--- aulapython.py
+++ aulapython.py
@@ -1,20 +1,34 @@
-print('Olá Mundo!')
-nome='jordy'
-idade= 20
-altura=165
-input('Digite seu nome: ')
-if nome =='jordy':
-    print('você acertou o nome! kk')
-elif nome != 'jordy':
-    print('Você errou o nome!')
+valor1 = int(input('Digite um número inteiro: '))
+valor2 = int(input ('Digite outro número inteiro: '))
 
-input('Digite sua idade: ')
-if idade ==20:
-    print('Você acertou a idade')
-elif idade != 20:
-    print('Você errou a idade!')
-input('Digite sua altura: ')
-if altura== 165:
-    print('Você acertou a altura')
-elif altura != 165:
-    print('Você errou a altura!')        
+print(valor1 == valor2)
+print(valor1 > valor2)
+print(valor1 < valor2)
+print(valor1 >= valor2)
+print(valor1 <= valor2)
+
+numero = 10
+print(numero>0 and numero<10)
+print(numero>0 or numero <10)
+
+valor3 =int(input('Digite um número: '))
+
+resto = valor3 % 2
+if resto==0:
+    print('o numero é par')
+else:
+    print('O numero é impar')    
+
+
+
+cupom = input('Digite o cupom: ')
+if (cupom == 'aula1' or cupom == 'aula2' ):
+    print('Voce ganhou 10% de desconto')
+else:
+    print ('cupom invalido')
+
+cupom = input ('Digite o cupom: ')
+if(cupom == 'aula1' or 'aula2' ):
+    print('Cupom válido, voce ganhou 15% de desconto')
+else:
+    print('voce errou o cupom mas ganhou 5% de desconto!')    
